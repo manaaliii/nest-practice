bab4069059d7a30201d76bf4730e875f6dc490a8
manaaliii <20bce304@nirmauni.ac.in>
2024-01-22 14:32:54 +0530

    modification uploaded

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Delete, Get, Param, Patch, Post, Query, Req, Res } from '@nestjs/common';
+import { Request } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+class AppController {
+    @Get()
+    getHello() {
+        return {
+                    name: 'user',
+                    class: 'first ',
+                    song: 'gurenge'
+                }
+    }
+    @Get('/good-bye')
+    getGoodbye(): string {
+        return 'Goodbye World!';
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    // @Get('/:userId')
+    // getUserId(@Param() params: {userId : number}){
+    //     return params.userId;
+    // }
+
+    @Get('/user')
+    getUserDetails(@Query() query: {userId: number, name: string, age:number}) {
+        console.log(query)
+        return query;
+
+    }
+      
+    @Patch('/update/:userId')
+    updateUser(@Param() params:{userId:number}, @Req() req: Request){
+        console.log(params)
+        console.log(req.body)
+        return params
+    }
+
+    @Delete('/delete/:userId')
+    deleteUser(@Param() params: {userId: number}) {
+        console.log(params)
+        return params.userId;
+    }
+
+    @Post()
+    getUser(@Req() req) {
+        return req.body;
+    }
+
+    @Get()
+    getUserObj() {
+        return {
+            name: 'user',
+            class: 'first ',
+            song: 'again'
+        }
+    } 
 }
+export { AppController };
